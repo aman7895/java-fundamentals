336844592a8d01686455eccd1e2213b3f3493541
Aman Deep Middha <aman.middha@gmail.com>
2021-01-09 16:56:56 +0800

    Update Exercise_01.java

--- src/labs_examples/datastructures/hashmap/labs/Exercise_01.java
+++ src/labs_examples/datastructures/hashmap/labs/Exercise_01.java
@@ -21,3 +21,62 @@ package labs_examples.datastructures.hashmap.labs;
  *  clear()
  *
  */
+
+import java.util.HashMap;
+
+public class Test {
+
+    HashMap<String, String> testHashMap;
+
+    public Test() {
+        testHashMap = new HashMap<>();
+    }
+
+    public void testHashMapFunctions() {
+        // put
+        testHashMap.put("x", "First entry");
+
+        // get
+        System.out.println(testHashMap.get("x"));
+
+        HashMap<String, String> secondTestMap = new HashMap<>();
+        secondTestMap.put("y", "First entry in second map");
+        secondTestMap.put("z", "Second entry in second map");
+
+        // putAll
+        testHashMap.putAll(secondTestMap);
+
+        // size
+        System.out.println(testHashMap.size());
+
+        // contains
+        if (testHashMap.containsKey("x")) {
+            System.out.println("x is in the map");
+        }
+
+        // entrySet
+        System.out.println(testHashMap.entrySet().size());
+
+        // keySet and forEach
+        testHashMap.keySet().forEach(System.out::println);
+
+        // putIfAbsent
+        testHashMap.putIfAbsent("x", "This won't be inserted.");
+
+        // remove
+        testHashMap.remove("z");
+
+        // replace
+        testHashMap.replace("x", "updating this value");
+
+        // clear
+        testHashMap.clear();
+
+    }
+
+    public static void main(String[] args) {
+        Test test = new Test();
+        test.testHashMapFunctions();
+        
+    }
+}
